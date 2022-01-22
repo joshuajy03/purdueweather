b00ed92601003817ed3ffcb7c83e4a8dd70b1418
Joshua Yang <joshuajy03@gmail.com>
2022-01-21 23:38:17 -0500

    merge

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,6 @@ import './WebPage.css';
 
 function Home() {
   return (
-<<<<<<< HEAD
     <div className="home" align = "left">
         <label>
           Input the temperature that you are comfortable in in Fahrenheit 
@@ -16,11 +15,6 @@ function Home() {
         <button>
           Submit
         </button> 
-=======
-    <div className="home">
-        <h1> hello</h1>
-        <h2> also displaying</h2>
->>>>>>> 404e00569f432bb9982afbdaeeefc20a167fad07
     </div>
   );
 }
