0031df09988fe5600d20dbf2f9e11e1962877168
Gao Changxiang <gao654@purdue.edu>
2022-01-22 10:01:24 -0500

    Placed elements in a red container, and aligned it to the center

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 import './WebPage.css';
-
+import SubmitButton from  './SubmitButton.jsx'
 
 function Home() {
   return (
-    <div className="home" align = "left">
-        <label>
-          Input the temperature that you are comfortable in in Fahrenheit 
-        </label>
-        <br></br>
-        <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">  
-        </input>
-        <br></br>
-        <button>
-          Submit
-        </button> 
+    <div className="home">
+        <div class = "container pt-3 pb-3 my-3" align = "center">
+          <label>
+            Input the temperature that you are comfortable in in Fahrenheit 
+          </label>
+          <br></br>
+          <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">  
+          </input>
+          <br></br>
+          <SubmitButton />
+        </div>  
     </div>
   );
 }
