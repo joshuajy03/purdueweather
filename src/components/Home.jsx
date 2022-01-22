c59dea3a42591f8b3604e8eeca193e08b77aa799
Gao Changxiang <gao654@purdue.edu>
2022-01-22 10:27:26 -0500

    Changed the welcome text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <div className="home">
         <div class = "container pt-3 pb-3 my-3" align = "center">
           <label>
-            Input the temperature that you are comfortable in in Fahrenheit 
+            Did you ever have the experience of stepping out of your house, just to realize that you have worn way too little? Sometimes, you may have even worn too much. Well not to fear, Purdue CS students got you covered! We have designed the perfect algorithm to calculate how many layers of clothes you need to stay comfortably toasty even in the harshest of winters! Just enter the temperature that you're most comfortable in and you're set. 
           </label>
           <br></br>
           <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">  
