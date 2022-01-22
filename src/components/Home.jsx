3b79d2f19a7eaf2aef88457e5e568ae5feea08f9
Sankhya Gowda <sankhyagowda@pal-10-184-118-173.itap.purdue.edu>
2022-01-22 11:08:09 -0500

    message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import './WebPage.css';
 import SubmitButton from  './SubmitButton.jsx'
+import './WebPage.css';
+
 
 function Home() {
   return (
@@ -8,21 +10,21 @@ function Home() {
         <div class = "container pt-3 pb-3 my-3" align = "center">
           <div class = "message welcome">
             <label>
-              Did you ever have the experience of stepping out of your house, just to realize that you have worn way too little? Sometimes, you may have even worn too much. Well not to fear, Purdue CS students got you covered! We have designed the perfect algorithm to calculate how many layers of clothes you need to stay comfortably toasty even in the harshest of winters! Just enter the temperature that you're most comfortable in and you're set. 
-            </label>  
+              Did you ever have the experience of stepping out of your house, just to realize that you have worn way too little? Sometimes, you may have even worn too much. Well not to fear, Purdue CS students got you covered! We have designed the perfect algorithm to calculate how many layers of clothes you need to stay comfortably toasty even in the harshest of winters! Just enter the temperature that you're most comfortable in and you're set.
+            </label>
           </div>
-          
+
           <br></br>
 
           <div class = "input temperature">
-            <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">  
+            <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">
             </input>
           </div>
-          
+
           <br></br>
 
           <SubmitButton />
-        </div>  
+        </div>
     </div>
   );
 }
