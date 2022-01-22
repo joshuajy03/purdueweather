914c637e83dae6fb9045d7e01c5af2b04d5cab70
Sankhya Gowda <sankhyagowda@pal-10-184-118-173.itap.purdue.edu>
2022-01-22 00:02:41 -0500

    change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,19 +14,6 @@ function NavBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" class="testClass">
         <Toolbar>
-<<<<<<< HEAD
-          <Typography variant="h6" class="navbar_buttons" component="div" sx={{ flexGrow: 1 }}>
-            Home
-          </Typography>
-          <Typography variant="h6" class="navbar_buttons" component="div" sx={{ flexGrow: 1 }}>
-            What we do
-          </Typography>
-          <Typography variant="h6" class="navbar_buttons" component="div" sx={{ flexGrow: 1 }}>
-            Contact Us
-          </Typography>
-          <Typography variant="h6" class="navbar_buttons" component="div" sx={{ flexGrow: 1 }}>
-            Our Services
-=======
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <nav>
               <Link to="/">Home</Link>
@@ -41,7 +28,6 @@ function NavBar() {
             <nav>
               <Link to="/">Contact Us</Link>
             </nav>
->>>>>>> a2d67585672e3f1ae17e9d8206950587e770403d
           </Typography>
 
           <Button color="inherit">Login</Button>
