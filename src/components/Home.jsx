e9080b9e28042de27b64e3e9db240bdcdf688336
Joshua Yang <joshuajy03@gmail.com>
2022-01-21 22:32:41 -0500

    framework

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import './WebPage.css';
+
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Home;
