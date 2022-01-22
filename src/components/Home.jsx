77f831823cd90b6954009ec5b29139d19057d79b
Gao Changxiang <gao654@purdue.edu>
2022-01-22 10:46:02 -0500

    Added padding between the elements on the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,21 @@ function Home() {
   return (
     <div className="home">
         <div class = "container pt-3 pb-3 my-3" align = "center">
-          <label>
-            Did you ever have the experience of stepping out of your house, just to realize that you have worn way too little? Sometimes, you may have even worn too much. Well not to fear, Purdue CS students got you covered! We have designed the perfect algorithm to calculate how many layers of clothes you need to stay comfortably toasty even in the harshest of winters! Just enter the temperature that you're most comfortable in and you're set. 
-          </label>
+          <div class = "message welcome">
+            <label>
+              Did you ever have the experience of stepping out of your house, just to realize that you have worn way too little? Sometimes, you may have even worn too much. Well not to fear, Purdue CS students got you covered! We have designed the perfect algorithm to calculate how many layers of clothes you need to stay comfortably toasty even in the harshest of winters! Just enter the temperature that you're most comfortable in and you're set. 
+            </label>  
+          </div>
+          
           <br></br>
-          <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">  
-          </input>
+
+          <div class = "input temperature">
+            <input type = "text" id = "comft_temp" placeholder = "Enter prefered temperature">  
+            </input>
+          </div>
+          
           <br></br>
+
           <SubmitButton />
         </div>  
     </div>
