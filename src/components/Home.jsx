7f9ed725716d870147cd067f598de478dcd3068c
Joshua Yang <joshuajy03@gmail.com>
2022-01-22 21:32:27 -0500

    switch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './WebPage.css';
-import SubmitButton from  './SubmitButton.jsx'
+import SubmitButton from './SubmitButton.jsx'
 import './WebPage.css';
 import { Link, withRouter } from "react-router-dom";
 import countries from 'i18n-iso-countries';
@@ -8,6 +8,10 @@ import background from "../bg.jpg";
 import sky from "../sky.jpeg";
 import Answers from "./Answers"
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
 
@@ -37,38 +41,51 @@ function Home() {
 
   return (
     <div className="home">
-        <div class = "container pt-3 pb-3 my-3"style={{ backgroundImage: `url(${sky})` }} align = "center">
-          <div class = "message" style={{ backgroundImage: `url(${sky})` }}>
+      <div class="container pt-3 pb-3 my-3" style={{ backgroundImage: `url(${sky})` }} align="center">
+        <div class="message" style={{ backgroundImage: `url(${sky})` }}>
 
           <label>
             Did you ever have the experience of stepping out of your house,
             just to realize that it's much more colder than you thought?
-              <br></br>  <br></br>
+            <br></br>  <br></br>
           </label>
-          </div>
-          <div class="message2" style={{ backgroundImage: `url(${sky})` }} >
-            <label>
+        </div>
+        <div class="message2" style={{ backgroundImage: `url(${sky})` }} >
+          <label>
             Sometimes, you may have even worn too much. Well not to fear,
-             Purdue CS students got you covered! We have designed the perfect
-             algorithm to calculate how many layers of clothes you need to
-             stay comfortably toasty even in the harshest of winters! Just
-         enter the temperature that you're most comfortable in and you're set.
-            </label>
-          </div>
+            Purdue CS students got you covered! We have designed the perfect
+            algorithm to calculate how many layers of clothes you need to
+            stay comfortably toasty even in the harshest of winters! Just
+            enter the temperature that you're most comfortable in and you're set.
+          </label>
+        </div>
 
-          <br></br>
+        <br></br>
 
-          <div class = "input temperature">
-            <input type = "number" id = "userInput" onChange={inputHandler} placeholder = "Enter preferred temperature">
-            </input>
-          </div>
+        
 
-          <br></br>
-        <Button variant="contained" onClick = {() => { setState(getState); setState2(true); }} >Submit
+        <div class="input temperature">
+        <Stack direction="row" justifyContent='center' spacing={1} alignItems="center">
+          <Typography>&deg;F</Typography>
+          <Switch defaultChecked />
+          <Typography>&deg;C</Typography>
+        </Stack>
+          <TextField
+            id="userInput"
+            onChange={inputHandler}
+            label="Enter Preferred Temperature"
+            type="number"
+            variant="filled"
+
+          />
+        </div>
+
+        <br></br>
+        <Button variant="contained" onClick={() => { setState(getState); setState2(true); }} >Submit
         </Button>
         {state2 ? displayAnswer() : displayNothing()}
-        </div>
-        
+      </div>
+
     </div>
   );
 }
